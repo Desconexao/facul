73cbc2eebb329a8d4c45b3278bbddc1d5ab091c0
Vinícius Lameirão <viniciuslameirao@outlook.com>
2025-05-07 15:03:20 -0300

    fixing

--- PC/excomplementares/22.c
+++ PC/excomplementares/22.c
@@ -7,10 +7,10 @@ int main() {
     printf("Digite um numero: ");
     scanf("%d", &x);
 
-    for (int i = 0; i < x; i++) {
+    for (int i = 1; i < x; i++) {
         printf("%d: ", i);
         for (int j = 0; j < x; j++) {
-            if (j != 0 && i % j == 0) printf("%dv", j);
+            if (j != 0 && i % j == 0) printf("%d ", j);
         }
         printf("\n");
     }
