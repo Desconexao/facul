a9a4be181a4151a976819e0c1e4ee1dccbede2be
Vinícius Lameirão <viniciuslameirao@hotmail.com>
2025-05-25 03:28:52 -0300

    prova1.c finished

--- PC/prova1.c
+++ PC/prova1.c
@@ -13,16 +13,28 @@ int main() {
   for (m = 0; m < 5; m++)
     for (s = 0; s < 5; s++)
       scanf("%d", &horas_de_frio[m][s]);
-  calcula(horas_de_frio, horas_de_frio_mes_media, perc_horas_de_frio_mes, &perc_total_horas_de_frio);
-  printf("Percentual de horas de frio por mes:\n");
 
+  calcula(horas_de_frio, horas_de_frio_mes_media, perc_horas_de_frio_mes, &perc_total_horas_de_frio);
+  
+  printf("Percentual de horas de frio por mes:\n");
   for (m = 0; m < 5; m++)
     printf("%.1f\n", perc_horas_de_frio_mes[m]);
+
   printf("Percentual do total de horas de frio em relacao a media:\n%.1f", perc_total_horas_de_frio);
 
   return 0;
 }
 
 void calcula(int horas_de_frio[5][5], int horas_de_frio_mes_media[5], float perc_horas_de_frio_mes[5], float *perc_total_horas_de_frio){
-
+  int somaFrioMes;
+  int somaFrioMeses = 0, somaFrioMesesMedia = 0;
+  for(int m = 0; m < 5; m++){
+    somaFrioMes = 0;
+    for(int s = 0; s < 5; s++)
+      somaFrioMes += horas_de_frio[m][s];
+    somaFrioMeses += somaFrioMes;
+    somaFrioMesesMedia += horas_de_frio_mes_media[m];
+    perc_horas_de_frio_mes[m] = (float) somaFrioMes / horas_de_frio_mes_media[m] * 100;
+  }
+  *perc_total_horas_de_frio = (float) somaFrioMeses / somaFrioMesesMedia * 100;
 }
