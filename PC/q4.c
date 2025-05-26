1a4743926c990b239533e8672d15fc62557ed067
Vinícius Lameirão <viniciuslameirao@outlook.com>
2025-05-26 09:38:14 -0300

    q4

--- PC/q4.c
+++ PC/q4.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int main() {
+
+void ex2() {
   int matriz[3][3] = {{1, 2, 3}, {4, 5}, {6}};
   int i, j;
 
@@ -10,6 +13,19 @@ int main() {
     }
     printf("\n");
   }
+}
 
-  return 0;
+void ex4(){
+  char s1[50] = "jack", s2[50] = "jill", s3[50], *sptr;
+
+  printf("%c%s ", toupper(s1[0]), &s1[1]);
+  printf("%s ", strcpy(s3, s2));
+  printf("%s ", strcat(strcat(strcpy(s3, s1), " and "), s2));
+  printf("%u ", strlen(s1) + strlen(s2));
+  printf("%u ", strlen(s3));
+}
+
+int main(){
+  //ex2
+  ex4();
 }
