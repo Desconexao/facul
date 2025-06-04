cb6aeaa29df16f2a88cc695355e9c9ecc67fa525
Vinícius Lameirão <viniciuslameirao@outlook.com>
2025-06-04 13:43:55 -0300

    teste.c

--- PC/teste.c
+++ PC/teste.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+void test1() {
     char *frase = {"Hello world"};
     printf("%s", frase);
+}
+
+void test2() {
+    int jump[] = {0, 2, 3, 4, 1235};
+
+    printf("%d", *(jump + 2));
+}
+
+int main(int argc, char *argv[]) {
+    test2();
     return 0;
 }
