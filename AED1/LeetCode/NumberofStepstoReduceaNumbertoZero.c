e9f00a9c0ef45974cfd94266b8744e9c98359ca0
Vinícius Lameirão <viniciuslameirao@outlook.com>
2025-10-03 20:23:11 -0300

    problem solved

---
+++ AED1/LeetCode/NumberofStepstoReduceaNumbertoZero.c
@@ -0,0 +1,12 @@
+int numberOfSteps(int num) {
+    int cont = 0;
+    while (num != 0) {
+        if ((num % 2) == 0) {
+            num /= 2;
+        } else {
+            num -= 1;
+        }
+        cont++;
+    }
+    return cont;
+}
